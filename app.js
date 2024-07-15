300e173370dfe10dd37c9ea9e8a046403f6e4cbe
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-07-15 17:29:03 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let greetingText = "Hi I'm Vishnu Vardhan Vankayalapati";
+let age = 32;
+alert(greetingText);
+greetingText = "I'm real Vishnu Vardhan Vankayalapati";
+alert(greetingText);
+alert(age);
