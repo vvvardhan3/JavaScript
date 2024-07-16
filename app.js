b890481b525afcdf814a5062cb8f469b363efd2c
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-07-16 07:18:33 +0530

    Creating a new element using JS

--- app.js
+++ app.js
@@ -2,3 +2,21 @@
 
 let anchorElement = document.getElementById("external-link");
 anchorElement.href = "https://google.com";
+
+// Adding a new element
+
+// 1. Create the new element
+
+let newAnchorElement = document.createElement("a");
+
+newAnchorElement.textContent = 'This leads to the google!';
+newAnchorElement.href = "https://google.com";
+
+// 2. Get access to the parent element that should hold the new element
+
+let firstParaGraph = document.querySelector('p');
+
+// 3. Insert the new element into the parent element content
+
+firstParaGraph.append(newAnchorElement);
+
