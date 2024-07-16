2ff9aaa4ed96c935a4f3093a31b3cf8bc6140fbc
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-07-16 21:39:48 +0530

    updated EventListener

---
+++ demo.js
@@ -0,0 +1,16 @@
+let productNameInputElement = document.getElementById('product-name');
+
+let remainingCharsElement =  document.getElementById('remaining-chars');
+
+let maxAllowedChars = productNameInputElement.maxLength; // MaxLength of the Input text
+
+function updateRemainingCharacters(event){
+    let enteredText = event.target.value;
+    let enteredTextLength = enteredText.length;
+    
+    let remainingCharacters = maxAllowedChars - enteredTextLength;
+
+    remainingCharsElement.textContent = remainingCharacters;
+}
+
+productNameInputElement.addEventListener('input',updateRemainingCharacters);
