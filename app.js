49e071094d05ad61e24c81ee15849229a6ecb11d
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-07-16 17:28:03 +0530

    addEventListener

--- app.js
+++ app.js
@@ -1,35 +1,18 @@
-// document.body.children[1].children[0].href = 'https://google.com';
+let paragraphElement = document.querySelector("p");
 
-let anchorElement = document.getElementById("external-link");
-anchorElement.href = "https://google.com";
+function changeParagraphText() {
+  paragraphElement.textContent = "Clicked!";
+  console.log("Paragraph Clicked!");
+}
 
-// Adding a new element
-// A. Create the new element
+paragraphElement.addEventListener("click", changeParagraphText);
 
-let newAnchorElement = document.createElement("a");
 
-newAnchorElement.textContent = "This leads to the google!";
-newAnchorElement.href = "https://google.com";
+let inputElement = document.querySelector('input');
 
-// B. Get access to the parent element that should hold the new element
+function retrieveUserInput() {
+    let enteredText = inputElement.value;
+    console.log(enteredText)
+}
 
-let firstParaGraph = document.querySelector("p");
-
-// C. Insert the new element into the parent element content
-
-firstParaGraph.append(newAnchorElement);
-
-// Removing element
-// A. Select the element that should be removed
-
-let firstH1Element = document.querySelector("h1");
-
-// B. Remove it
-
-firstH1Element.remove();
-
-// InnerHTML
-
-// InnerHTML is Different to textContent function.
-
-firstParaGraph.innerHTML = 'Hi EveryOne, A <strong>small reminder</strong> to everyOne around here!'
+inputElement.addEventListener('input',retrieveUserInput);
