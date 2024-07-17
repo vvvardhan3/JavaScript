5962788f0c9911092ec592dbe6e652ef72fa87b7
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-07-17 22:59:28 +0530

    feature updated

--- demo.js
+++ demo.js
@@ -5,12 +5,25 @@ const remainingCharsElement = document.getElementById("remaining-chars");
 const maxAllowedChars = productNameInputElement.maxLength; // MaxLength of the Input text
 
 function updateRemainingCharacters(event) {
-    const enteredText = event.target.value;
-    const enteredTextLength = enteredText.length;
+  const enteredText = event.target.value;
+  const enteredTextLength = enteredText.length;
 
-    const remainingCharacters = maxAllowedChars - enteredTextLength;
+  const remainingCharacters = maxAllowedChars - enteredTextLength;
 
   remainingCharsElement.textContent = remainingCharacters;
+
+  if (remainingCharacters === 0) {
+    remainingCharsElement.classList.add("error");
+    productNameInputElement.classList.add("error");
+  } else if (remainingCharacters <= 10) {
+    remainingCharsElement.classList.add("warning");
+    productNameInputElement.classList.add("warning");
+    remainingCharsElement.classList.remove("error");
+    productNameInputElement.classList.remove("error");
+  } else {
+    remainingCharsElement.classList.remove("warning");
+    productNameInputElement.classList.remove("warning");
+  }
 }
 
 productNameInputElement.addEventListener("input", updateRemainingCharacters);
