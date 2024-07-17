d9ef4a96b4953d2a4de8d4a39b087858b0cf1fb5
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-07-18 01:11:29 +0530

    Second Feature added!

--- loops-in-action/loops-in-action.js
+++ loops-in-action/loops-in-action.js
@@ -3,19 +3,38 @@
 const calculateSumButtonElement = document.querySelector("#calculator button");
 
 function calculateSum() {
-    const userNameInputElement = document.querySelector('#user-number');
-    const enteredNumber = userNameInputElement.value;
+  const userNameInputElement = document.querySelector("#user-number");
+  const enteredNumber = userNameInputElement.value;
 
-    let sumUpToNumber = 0;
+  let sumUpToNumber = 0;
 
-    for(let i = 0; i <=enteredNumber; i++) {
-        sumUpToNumber = sumUpToNumber + i;
-    }
+  for (let i = 0; i <= enteredNumber; i++) {
+    sumUpToNumber = sumUpToNumber + i;
+  }
 
-    const outputResultElement = document.querySelector('#calculated-sum');
+  const outputResultElement = document.querySelector("#calculated-sum");
 
-    outputResultElement.textContent = sumUpToNumber;
-    outputResultElement.style.display = 'block';
+  outputResultElement.textContent = sumUpToNumber;
+  outputResultElement.style.display = "block";
 }
 
 calculateSumButtonElement.addEventListener("click", calculateSum);
+
+// Highlight links
+
+const highlightLinksButtonElement = document.querySelector(
+  "#highlight-links button"
+);
+
+function highlightLinks() {
+  const anchorElements = document.querySelectorAll("#highlight-links a");
+
+  for (const anchorElement of anchorElements) {
+    anchorElement.classList.add("highlight");
+  }
+}
+
+highlightLinksButtonElement.addEventListener("click", highlightLinks);
+
+
+// 
