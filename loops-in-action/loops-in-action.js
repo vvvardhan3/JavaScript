48f889d0ce7816b820330197c4f001dfd8135cc7
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-07-18 01:25:06 +0530

    Third Feature Update

--- loops-in-action/loops-in-action.js
+++ loops-in-action/loops-in-action.js
@@ -21,7 +21,6 @@ function calculateSum() {
 calculateSumButtonElement.addEventListener("click", calculateSum);
 
 // Highlight links
-
 const highlightLinksButtonElement = document.querySelector(
   "#highlight-links button"
 );
@@ -36,5 +35,27 @@ function highlightLinks() {
 
 highlightLinksButtonElement.addEventListener("click", highlightLinks);
 
+// Display user data
 
-// 
+const dummyUserData = {
+  firstName: "Vishnu Vardhan",
+  LastName: "Vankayalapati",
+  Age: "25",
+};
+
+const displayUserDataButtonElement = document.querySelector("#user-data");
+
+function displayUserData() {
+  const outputDataElement = document.querySelector("#output-user-data");
+
+  outputDataElement.innerHTML = '';
+
+  for (const key in dummyUserData) {
+    const newUserData = document.createElement("li");
+    const outputText = key + ": " + dummyUserData[key].toUpperCase();
+    newUserData.textContent = outputText;
+    outputDataElement.append(newUserData);
+  }
+}
+
+displayUserDataButtonElement.addEventListener("click", displayUserData);
