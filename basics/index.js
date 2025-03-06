76aacc093be9e6b4eae9964e396abe90929938a7
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2025-03-06 06:29:05 +0530

    swiping two values

---
+++ basics/index.js
@@ -0,0 +1,36 @@
+// let a = 10
+// let b = 20
+
+// console.log("Sum of 10 and 20 : ", a + b);
+
+// type coercion
+// console.log("1" + 1);
+// console.log("1" - 1);
+// console.log("1" * 1);
+// console.log("1" % 1);
+
+// type casting
+let age = "Vishnu"
+
+age = Number(age)
+
+console.log(age); // Nan
+
+
+// Swiping two numbers
+
+let a = 10
+let b = 20
+
+// let temp = a
+// a = b
+// b = temp 
+
+// console.log(a, b);
+
+a = a + b // 30
+b = a - b // 10
+a = a - b // 20
+
+
+console.log(a,b)
